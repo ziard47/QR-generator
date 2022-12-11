8d9d6835d9ec75320bc230215e83d7063a21684e
ziard <61586725+ziard47@users.noreply.github.com>
2022-12-12 00:06:41 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace QRCodeGenerator
+{
+    partial class QRGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(120, 464);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(265, 42);
+            this.btnGen.TabIndex = 0;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(108, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 288);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(33, 425);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(455, 20);
+            this.txtText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 55);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "QR Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Url or Text:";
+            // 
+            // QRGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 519);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGen);
+            this.Name = "QRGen";
+            this.Text = "QR Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace QRCodeGenerator
+{
+    public partial class QRGen : Form
+    {
+        public QRGen()
+        {
+            InitializeComponent();
+        }
+        private Bitmap GenerateQRCode(string text)
+        {
+            var url = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=" + text;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var stream = response.GetResponseStream();
+            return new Bitmap(stream);
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            var text = txtText.Text;
+            var qrCode = GenerateQRCode(text);
+            pictureBox1.Image = qrCode;
+        }
+    }
+}
